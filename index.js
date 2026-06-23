fddbb6380a39d0dce060307132eb92caf24cd2dd
Nicolas <nicolasmatriiano@gmail.com>
2026-06-23 17:26:04 -0300

    Liberando CORS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 
